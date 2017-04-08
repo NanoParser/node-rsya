134a2dbd838ded88fa8062b5ca940307157d1ac3
Sergey <nanoparser@gmail.com>
2017-04-08 20:27:04 +0300

    Promises demo

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var unique = require('array-unique');
 var path = require('path'); // to work with files
+const winston = require('winston');
+
 var app = express();
 
 // parse application/x-www-form-urlencoded 
@@ -21,80 +23,45 @@ var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : ''
+  password : '',
+  database: 'rsya'
 });
 
-// Описываем объект типа Карта (базовый РСЯ объект без домена)
-function yaCard(title, image, text){
-  this.title = title;
-  this.image = image;
-  this.text = text;
+function getDomains()
+{
+    return new Promise(function(resolve, reject) {
+        // The Promise constructor should catch any errors thrown on
+        // this tick. Alternately, try/catch and reject(err) on catch.
+
+        var query_str =
+            "SELECT name FROM domains";
+
+        connection.query(query_str, function (err, rows, fields) {
+            // Call reject on error states,
+            // call resolve with results
+            if (err) {
+                return reject(err);
+            }
+            resolve(rows);
+        });
+    });
 }
 
+getDomains()
+    .then(rows => {
+        let collection = {
 
-
-//function t (callback) {
-var domains = [];
-
-connection.query('SELECT `id`,`name` FROM `rsya`.`domains`;', function(err, rows, fields) {
-
-		if (err) throw err;
-		for(var i=0; i<rows.length; i++)
-		{
-			var domain = {};
-			domain.name = rows[i].name;
-			domain.id = rows[i].id;
-			domain.cards = [];
-			var query = 'SELECT `title`,`image`,`text` FROM `rsya`.`cards` WHERE `domain-id`='+ rows[i].id +';';
-			connection.query(query, function(err, rows, fields) {
-				if (err) throw err;
-				for(var i=0; i<rows.length; i++)
-				{
-					var card = new yaCard(rows[i].title, rows[i].image, rows[i].text);
-					domain.cards.push(card); 
-				}
-			});
-			domains[i] = domain;
-		}
-	
-//	callback(domains);
-
-});
-
-//}
-/*
-t(function(domains){
-	console.log(domains.length);
-
-	for (var i = 0; i < domains.length; i++)
-	{
-
-		connection.query('SELECT `title`,`image`,`text` FROM `rsya`.`cards` WHERE `domain-id`='+ domains[i].id +';', function(err, rows, fields) {
-		
-			if (err) throw err;
-			for(var j=0; j<rows.length; j++)
-			{
-				var card = {};
-				card.title = rows[j].title;
-				domains[i].cards.push(card);
-			}
-		});
-
-	}
-
-});
-*/
+        };
+//        winston.log('info', rows);
+        for(var i = 0; i<rows.length; i++){
+            collection.domains.push(rows[i].name);
+        }
+//        winston.log('info', collection);
+        return collection;
+    });
 
 app.get('/', function(req, res) {
     res.render('test.ejs', {'domains': domains});
 });
 
 app.listen(3000, '0.0.0.0');
-
-/*
-
-server.listen(80, '0.0.0.0');
-
-*/
-
-//console.log(titles);
