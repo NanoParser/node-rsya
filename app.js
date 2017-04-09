68d8ec7939442a754cb66ba9ba7f777e826face1
Sergey <nanoparser@gmail.com>
2017-04-09 11:15:14 +0300

    Promises variations

--- app.js
+++ app.js
@@ -42,6 +42,33 @@ function getDomains()
             if (err) {
                 return reject(err);
             }
+ //           winston.log('info', rows);
+            resolve(rows);
+        });
+    });
+}
+
+function getCards(domain)
+{
+    return new Promise(function(resolve, reject) {
+        // The Promise constructor should catch any errors thrown on
+        // this tick. Alternately, try/catch and reject(err) on catch.
+
+//        winston.log('info', domain);
+
+        var query_str =
+            "SELECT title, image, text FROM cards WHERE `domain-id` =" +
+            " (SELECT id FROM domains WHERE `name`='" + domain + "')";
+
+ //       winston.log('info', query_str);
+
+        connection.query(query_str, function (err, rows, fields) {
+            // Call reject on error states,
+            // call resolve with results
+            if (err) {
+                return reject(err);
+            }
+//            winston.log('info', rows);
             resolve(rows);
         });
     });
@@ -49,16 +76,39 @@ function getDomains()
 
 getDomains()
     .then(rows => {
-        let collection = {
-
-        };
-//        winston.log('info', rows);
+        var domains = [];
         for(var i = 0; i<rows.length; i++){
-            collection.domains.push(rows[i].name);
+            let domain = {name:undefined, cards:[]};
+            domain.name = rows[i].name;
+            domains[i] = domain;
         }
-//        winston.log('info', collection);
-        return collection;
-    });
+ //       winston.log('info', 'domains', domains);
+        return domains;
+    })
+    .then(domains => {
+        for(var i = 0; i<domains.length; i++)
+        {
+            domains[i].cards.push(
+                getCards(domains[i].name)
+                .then(
+                    rows => {
+                                let collection = [];
+                                for(var j = 0; j<rows.length; j++){
+                                    let card = {title:undefined, image:undefined, text:undefined};
+                                    card.title = rows[j].title;
+                                    card.image = rows[j].image;
+                                    card.text = rows[j].text;
+                                    collection[j] = card;
+                                }
+                               return collection;
+                            }
+                )
+        );
+        winston.log('info', domains);
+        }
+        //winston.log('info', domains);
+        return domains;
+});
 
 app.get('/', function(req, res) {
     res.render('test.ejs', {'domains': domains});
